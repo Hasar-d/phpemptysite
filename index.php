39076905448d7916403807287d6f9dd6f4005bfc
Hasar-d <ds.djahandideh@gmail.com>
2017-03-31 16:06:35 +0200

    Update index.php
    blabla

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-
+<div id="welcome">
+    <h1><span>Welcome to</span> Symfony on <span style="color:#00BCF2">Azure</span> {{ constant('Symfony\\Component\\HttpKernel\\Kernel::VERSION') }}</h1>
+</div>
 ?>
